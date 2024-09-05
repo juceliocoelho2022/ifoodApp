f489f1007c0b9129840dc2cdcf8df0939fe5433c
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2024-09-05 12:49:21 -0300

    Package Food

---
+++ app/src/main/java/com/devjucelio/ifoodapp/list_item/FoodItem.kt
@@ -0,0 +1,20 @@
+package com.devjucelio.ifoodapp.list_item
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun FoodItem(
+
+){
+
+
+    Constr
+}
+
+
+@Composable
+@Preview
+private fun FoodItemPreview(){
+    FoodItem()
+}
